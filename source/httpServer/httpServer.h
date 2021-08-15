528d02697718e790b554a01153358593a8a96934
Timo Schrader <timoschrader00@gmail.com>
2021-08-16 01:56:08 +0200

    feat(httpServer): Added static serve option

--- source/httpServer/httpServer.h
+++ source/httpServer/httpServer.h
@@ -2,6 +2,8 @@
 #define HTTPSERVER_H
 
 #include <list>
+#include <filesystem>
+#include <fstream>
 
 #include "../socketBase/socketBase.h"
 #include "../connectionBase/connectionBase.h"
@@ -16,15 +18,22 @@ namespace kleins
         std::list<std::unique_ptr<socketBase>> sockets;
 
         std::map<std::string,const std::function<void(httpParser*)>> functionTable;
-
+        std::map<std::string,std::string> fileLookup;
 
         void newConnection(connectionBase* conn);
+
+
+        static std::map<std::string,std::string> mimeLookup;
+
     public:
         httpServer(/* args */);
         ~httpServer();
 
         void on(const std::string& method,const std::string& uri, const std::function<void(httpParser*)> callback);
 
+        void serve(const std::string& uri, const std::string& path);
+
+        void serveDirectory(const std::string& baseuri, const std::string& path, const std::string indexFile = "index.html");
 
         bool addSocket(socketBase* socket);
     };
